d4620b96b9039e90307ad054882426a20a746a20
Jason L Perry <jason@suncoast.io>
2018-10-12 16:11:29 -0400

    Include user in json

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -2,6 +2,7 @@ class GifsController < ApplicationController
   def index
     @veneration = Veneration.where(score: params[:score]).order("RANDOM()").first
     render json: {
+      contributor: @veneration.user.name,
       caption: @veneration.caption,
       image: url_for(@veneration.image),
     }
