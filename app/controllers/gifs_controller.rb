eb519fd6c8e16375ff7d27da84290c353bea265b
Gavin Stark <gavin@gstark.com>
2020-05-18 22:10:22 -0400

    Adds support for a max byte size

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,11 +1,23 @@
 class GifsController < ApplicationController
   def index
-    @veneration = Veneration.where(score: params[:score]).order("RANDOM()").first
-    render json: {
-      contributor: @veneration.user.name,
-      caption: @veneration.caption,
-      image: url_for(@veneration.image),
-      url: url_for(@veneration)
-    }
+    venerations = Veneration.where(score: params[:score])
+
+    if params[:max_byte_size]
+      max_byte_size = params[:max_byte_size].to_i
+      venerations = venerations.joins(:image_blob).where("active_storage_blobs.byte_size < ?" , max_byte_size)
+    end
+
+    veneration = venerations.order("RANDOM()").first
+
+    if veneration
+      render json: {
+        contributor: veneration.user.name,
+        caption: veneration.caption,
+        image: url_for(veneration.image),
+        url: url_for(veneration)
+      }
+    else
+      head :no_content
+    end
   end
 end
