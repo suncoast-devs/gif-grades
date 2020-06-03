b068b27e3c310da0fda60f48e29ff15bc2c2a576
Gavin Stark <gavin@gstark.com>
2020-06-03 16:01:41 -0400

    Add a parameter to limit based on content type

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,12 +1,20 @@
 class GifsController < ApplicationController
   def index
-    venerations = Veneration.where(score: params[:score])
+    # Get venerations only where we have an actual gif content type
+    venerations = Veneration.where(score: params[:score]).joins(:image_blob)
 
-    if params[:max_byte_size]
-      max_byte_size = params[:max_byte_size].to_i
-      venerations = venerations.joins(:image_blob).where("active_storage_blobs.byte_size < ?" , max_byte_size)
+    # If we specify a particular content-type then apply that restriction too
+    if params[:content_type]
+      venerations = venerations.where("active_storage_blobs.content_type = ?", params[:content_type])
     end
 
+    # Limit them to a max_byte_size if specified
+    if params[:max_byte_size]
+      max_byte_size = params[:max_byte_size].to_i
+      venerations = venerations.where("active_storage_blobs.byte_size < ?", max_byte_size)
+    end
+
+    # And get a random image
     veneration = venerations.order("RANDOM()").first
 
     if veneration
@@ -14,7 +22,7 @@ class GifsController < ApplicationController
         contributor: veneration.user.name,
         caption: veneration.caption,
         image: url_for(veneration.image),
-        url: url_for(veneration)
+        url: url_for(veneration),
       }
     else
       head :no_content
